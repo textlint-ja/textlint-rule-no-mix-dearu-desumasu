d70089c69ba25a75e5ed3311e298df5ad29ef57f
azu <azuciao@gmail.com>
2015-10-10 00:39:38 +0900

    refactor(analyze): curve out analyze to module

--- src/no-mix-dearu-desumasu.js
+++ src/no-mix-dearu-desumasu.js
@@ -1,34 +1,10 @@
 // LICENSE : MIT
 "use strict";
 import {RuleHelper} from "textlint-rule-helper";
-
-function countMatchContent(text, reg) {
-    let count = 0;
-    let matches = text.match(reg) || [];
-    count += matches.length;
-    return {count, matches};
-}
-function countMatchContentEnd(text, reg) {
-    let count = 0;
-    let lines = text.split(/\r\n|\r|\n|\u2028|\u2029/g);
-    let matches = [];
-    lines.forEach(line => {
-        let match = line.match(reg) || [];
-        matches = matches.concat(match);
-        count += matches.length;
-    });
-    return {count, matches};
-}
+import analyze from "./analyze.js";
 export default function noMixDearuDesumasu(context) {
     let {Syntax, RuleError, report, getSource} = context;
     let helper = new RuleHelper(context);
-    // This RegExp come from https://github.com/recruit-tech/redpen/blob/master/redpen-core/src/main/java/cc/redpen/validator/sentence/JapaneseStyleValidator.java
-    const DEARU_PATTERN = /のだが|ないかと|してきた|であるから/g;
-    const DEARU_END_PATTERN = /(?:だ|である|った|ではない｜ないか|しろ|しなさい|いただきたい|いただく|ならない|あろう|られる)(?:[。]?)$/;
-
-    const DESUMASU_PATTERN = /でしたが|でしたので|ですので|ですが/g;
-    const DESUMASU_END_PATTERN = /(?:です|ます|ました|ません|ですね|でしょうか|ください|ませ)(?:[。]?)$/;
-
     let dearuCount = 0;
     let desumasuCount = 0;
 
@@ -48,22 +24,19 @@ export default function noMixDearuDesumasu(context) {
             let beforeDearuCount = dearuCount;
             let beforeDesumasuCount = desumasuCount;
             let text = getSource(node);
-            let matchDearu = countMatchContent(text, DEARU_PATTERN);
-            let matchDearuEnd = countMatchContentEnd(text, DEARU_END_PATTERN);
-            let matchDesumasu = countMatchContent(text, DESUMASU_PATTERN);
-            let matchDesumasuEnd = countMatchContentEnd(text, DESUMASU_END_PATTERN);
-            dearuCount += matchDearu.count + matchDearuEnd.count;
-            desumasuCount += matchDesumasu.count + matchDesumasuEnd.count;
+            let {desu,dearu} = analyze(text);
+            dearuCount += dearu.count;
+            desumasuCount += desu.count;
             if (beforeDearuCount !== dearuCount) {
                 dearuLastHit = {
                     node,
-                    matches: matchDearu.count > 0 ? matchDearu.matches : matchDearuEnd.matches
+                    matches: dearu.matches
                 };
             }
             if (beforeDesumasuCount !== desumasuCount) {
                 desumasuLastHit = {
                     node,
-                    matches: matchDesumasu.count > 0 ? matchDesumasu.matches : matchDesumasuEnd.matches
+                    matches: desu.matches
                 };
             }
         },
